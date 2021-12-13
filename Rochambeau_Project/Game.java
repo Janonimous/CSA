df9b15ed5939f402567a89bfe46e5d716af9d490
Jaynako <79431682+Janonimous@users.noreply.github.com>
2021-12-12 23:57:18 -0800

    Update Game.java

--- Rochambeau_Project/Game.java
+++ Rochambeau_Project/Game.java
@@ -7,8 +7,23 @@ class Game {
         Scanner input = new Scanner(System.in);
         CustomInput cinput = new CustomInput(false);
         
-        System.out.println("Ready to play Rock Paper Scissors? [y/n]");
-        boolean ready = cinput.yesNo(input.nextLine());
-        System.out.println(ready);
+        boolean ready;
+        do {
+            System.out.println("Ready to play Rock Paper Scissors? [y/n]");
+            ready = cinput.yesNo(input.nextLine());
+            if(ready) {
+                System.out.println("Would you like instructions for the this game? [y/n]");
+                if(cinput.yesNo(input.nextLine())) {
+                    /* To be implemented; Provide instructions on how to play this version of RPS
+                       then re-ask if they are ready to play the game */
+                }
+                /* To be implemented */
+            } else {
+                System.out.println("Would you like to quit the game? [y/n]");
+                if(cinput.yesNo(input.nextLine())) break;
+            }
+        } while(ready);
+
+        input.close();
     }
 }
